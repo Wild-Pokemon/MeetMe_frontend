3604b55585be8c1a21a7ef936e8ed9839bc42d0c
kyungmin <kyungmin727@gmail.com>
2024-06-04 16:40:21 +0900

    🚧 캘린더 작업중

--- src/pages/calender/Calender.jsx
+++ src/pages/calender/Calender.jsx
@@ -1,16 +1,62 @@
-import { useState } from "react";
-import { getYear, getMonth, getDate, getDay } from "date-fns";
+import { useCallback, useMemo, useState } from "react";
+import {
+  getYear,
+  subMonths,
+  addMonths,
+  format,
+  startOfMonth,
+  startOfWeek,
+  endOfWeek,
+  endOfMonth,
+  differenceInCalendarDays,
+  addDays,
+} from "date-fns";
 
 function Calender() {
-  const [currentData, setCurrentData] = useState(new Date());
-  const year = getYear(currentData);
-  const month = getMonth(currentData);
-  console.log(year);
+  const [currentDate, setCurrentDate] = useState(new Date());
 
-  const week = ["일", "월", "화", "수", "목", "금", "토"];
+  const year = getYear(currentDate);
+  const monthStart = startOfMonth(currentDate);
+  const monthEnd = endOfMonth(currentDate);
+  const startDate = startOfWeek(monthStart);
+  const endDate = endOfWeek(monthEnd);
+
+  const createMonth = useMemo(() => {
+    const monthArray = [];
+    let day = startDate;
+    while (differenceInCalendarDays(endDate, day) >= 0) {
+      monthArray.push(day);
+      day = addDays(day, 1);
+    }
+    return monthArray;
+  }, [startDate, endDate]);
+
+  const day = createMonth.map((item, i) => <p key={i}>{format(item, "d")}</p>);
+
+  const prev = useCallback(() => {
+    setCurrentDate(subMonths(currentDate, 1));
+  }, [currentDate]);
+
+  const next = useCallback(() => {
+    setCurrentDate(addMonths(currentDate, 1));
+  }, [currentDate]);
+
+  const weekList = ["일", "월", "화", "수", "목", "금", "토"];
+  const week = weekList.map((item, i) => <p key={i}>{item}</p>);
+
   return (
     <div>
-      {year}년{month}월
+      <button type="button" onClick={prev}>
+        이전달
+      </button>
+      <p>
+        {year}년{format(currentDate, "M")}월
+      </p>
+      <button type="button" onClick={next}>
+        다음달
+      </button>
+      {week}
+      {day}
     </div>
   );
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
         element: <FriendListPage />,
       },
       {
-        path: "calendar",
+        path: "calender",
         element: <Calender />,
       },
     ],
