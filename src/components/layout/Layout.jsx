285f846397ef43af45562c7b773593c4f1002f22
kyungmin <kyungmin727@gmail.com>
2024-06-21 15:55:38 +0900

    🚧 [FE] cont: 메인페이지 ui작업중

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import Header from "@components/layout/Header";
 import "@styles/components/layout/Layout.scss";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
+  const [user, setUser] = useState(false);
   return (
     <div className="layout">
-      <Header />
+      {user ? <Header /> : ""}
       <Outlet />
     </div>
   );
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,40 @@
+import styles from "@styles/MainPage.module.scss";
+import mainLogo from "@assets/mainpage_logo.png";
+import mainImg from "@assets/mainpage_image.svg";
+import Button from "@components/Button";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function MainPage() {
-  return <div>hello</div>;
+  const navigate = useNavigate();
+  const [user, setUser] = useState(true);
+
+  const handleLogin = () => {
+    navigate("/users/login");
+  };
+
+  return (
+    <>
+      {user ? (
+        <div className="dd">안녕</div>
+      ) : (
+        <div className={styles.main_wrapper}>
+          <div className={styles.mainBlur}>
+            <div className={styles.mainBox}>
+              <img src={mainLogo} alt="MeetMe_Logo" />
+              <img src={mainImg} alt="메인페이지 이미지아이콘" />
+              <Button
+                text="로그인하여 시작하기"
+                size="large"
+                className={styles.button}
+                onClick={handleLogin}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default MainPage;
